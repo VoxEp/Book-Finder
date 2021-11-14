e39335f2826d992fb02cd2150e4be01f978ce5cf
Vinicius R <voxep.dev@gmail.com>
2021-11-14 19:30:39 -0300

    flask setup changes

--- application.py
+++ application.py
@@ -1,4 +1,18 @@
-from app import *
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/book', methods=['GET','POST'])
+def book():
+    if request.method == "POST":
+        print("REQUESTED BY POST METHOD")
+        bookUrl = request.form.get("bookUrl")
+        return render_template("book.html", bookUrl=bookUrl)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
